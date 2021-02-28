9c9aa6ef52d8c5a7fe2677300cb5e0a7b95c2b65
Danish zahid <danish.zahid89@gmail.com>
2021-02-28 09:53:30 +0000

    initial spring starter peoject setup

---
+++ src/main/java/com/accela/coding/exercise/AccelaCodingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.accela.coding.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccelaCodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccelaCodingExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accela/coding/exercise/AccelaCodingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accela.coding.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccelaCodingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
