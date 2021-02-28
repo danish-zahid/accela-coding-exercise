dc6b48a11db3866c9433990bf0eb61a3fb42842d
Danish zahid <danish.zahid89@gmail.com>
2021-02-28 19:24:36 +0000

    Transactional configurations and documentation

---
+++ src/main/java/com/accela/coding/exercise/configuration/HibernateConfiguration.java
@@ -0,0 +1,9 @@
+package com.accela.coding.exercise.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfiguration {
+}
